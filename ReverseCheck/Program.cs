db2cf9d9c65a3e63ce35fd8f0acf01ef5338f1ef
Daniel Alvarez <danipoal.0@gmail.com>
2024-09-02 13:27:37 +0200

    Agregar archivos de proyecto.

---
+++ ReverseCheck/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseCheck
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("MENU DE ACCIONES");
+            Console.WriteLine("1 - Comprobar si la palabra es capicua");
+
+            string opcion = Console.ReadLine();
+            int opcionNumeral = 0;
+            try
+            {
+                opcionNumeral = int.Parse(opcion);
+            }catch (Exception e){
+                Console.WriteLine(e.Message);
+            }
+            
+            if (opcionNumeral == 1)
+            {
+                Console.WriteLine("Que palabra quieres comprobar? :");
+                string palabra = Console.ReadLine();
+                Console.WriteLine(IsReverse(palabra));
+            }
+        }
+        public static Boolean IsReverse(string palabra){
+            string inversa = "";
+            for (int i = palabra.Length-1; i >= 0; i--){
+                char c = palabra[i];
+                inversa += c.ToString();
+
+            }
+            if (palabra == inversa){
+                Console.WriteLine("Es capicua!");
+                return true;
+            }else{
+                Console.WriteLine("No es capicua!");
+                return false;
+            }
+        }
+        //TODO Transformar a CamelCase, Comparar fechas, IsBlankSpace, isLowerCase
+    }
+}
---
+++ ReverseCheck/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("ReverseCheck")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ReverseCheck")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("a124ffc4-bace-4597-bdf6-1804909acc37")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
