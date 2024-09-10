4d8ede491ef441f4b98d6af6d42de16f561a5304
Daniel Alvarez <danipoal.0@gmail.com>
2024-09-10 12:25:40 +0200

    +snakeLength

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -5,32 +5,47 @@ namespace Snake
 {
     internal class Program
     {
-        static char?[,] arraySnake = new char?[10, 10];
-        static (int x, int y) lastPosition = (5, 5);
+        static (int x, int y) dimension = (10, 10);
+        static char?[,] arraySnake = new char?[dimension.x, dimension.y];
+        static (int x, int y) headPosition = (5, 5);
         static List<Tuple<int,int>> savedPosition =new List<Tuple<int, int>>();
-        static bool isOver = false;
-        static int longitud = 3;
+        static bool isGameOver = false, isEnergy = false;
+        static int snakeLength = 3;
         static void Main(string[] args)
         {
+            //TODO Implementar enum + diccionario para el cuerpo del snake, energia y null
             Iniciar();
         }
 
         private static void Iniciar()
         {
             GenerarSnake();
-            while (!isOver)
+            (int x, int y) energyPosition = GenerarEnergia(); //bool false to true
+            while (!isGameOver)
             {
                 ImprimirTablero();
                 MoverSnake();
+                checkEnergyEat(energyPosition);
+                if (!isEnergy)
+                    energyPosition = GenerarEnergia();
+            }
+        }
+
+        private static void checkEnergyEat((int x, int y)energyPos)
+        {
+            if (energyPos == headPosition)
+            {
+                snakeLength++;
+                isEnergy = false;
             }
         }
 
         private static void GenerarSnake()
         {
-            for (int i = longitud - 1; i >= 0; i--)
+            for (int i = snakeLength - 1; i >= 0; i--)
             { 
-                arraySnake[lastPosition.x, lastPosition.y - i] = '@';
-                savedPosition.Add(new Tuple<int,int> (lastPosition.x, lastPosition.y - i));
+                arraySnake[headPosition.x, headPosition.y - i] = '@';
+                savedPosition.Add(new Tuple<int,int> (headPosition.x, headPosition.y - i));
             }
             
         }
@@ -43,16 +58,16 @@ namespace Snake
             switch (keyInfo.Key)
             {
                 case ConsoleKey.UpArrow:
-                    lastPosition.y--;
+                    headPosition.y--;
                     break;
                 case ConsoleKey.DownArrow:
-                    lastPosition.y++;
+                    headPosition.y++;
                     break;
                 case ConsoleKey.LeftArrow:
-                    lastPosition.x--;
+                    headPosition.x--;
                     break;
                 case ConsoleKey.RightArrow:
-                    lastPosition.x++;
+                    headPosition.x++;
                     break;
                 case ConsoleKey.Escape:
                     Console.WriteLine("ESC presionado, saliendo...");
@@ -63,13 +78,13 @@ namespace Snake
             }
             try
             {
-                if (arraySnake[lastPosition.x, lastPosition.y] == '@')  //Comprobanos si habia ya la casilla marcada, antes de marcarla
+                if (arraySnake[headPosition.x, headPosition.y] == '@')  //Comprobanos si habia ya la casilla marcada, antes de marcarla
                 {
                     ImprimirTablero();
                     GameOver();
                 }
 
-                arraySnake[lastPosition.x, lastPosition.y] = '@';
+                arraySnake[headPosition.x, headPosition.y] = '@';
                 UpdateSavedPositions();
             }
             catch (IndexOutOfRangeException ex)
@@ -84,22 +99,49 @@ namespace Snake
 
         private static void UpdateSavedPositions()
         {
-            savedPosition.Add(new Tuple<int, int>(lastPosition.x, lastPosition.y));
             var lastPoint = savedPosition[0];
-            (int x, int y) pointToRemove = (lastPoint.Item1, lastPoint.Item2);
-            RemoveSnake(pointToRemove);
-            savedPosition.RemoveAt(0);
+            savedPosition.Add(new Tuple<int, int>(headPosition.x, headPosition.y));
+
+            //Eliminamos el ultimo punto del snake
+            if (savedPosition.Count == snakeLength + 1)
+            {
+                arraySnake[lastPoint.Item1, lastPoint.Item2] = null;
+                savedPosition.Remove(lastPoint);
+            }
+
         }
 
-        private static void RemoveSnake((int x, int y) punto)
+
+        private static (int, int) GenerarEnergia()
         {
-            arraySnake[punto.x, punto.y] = null;
+            //Tiene que ser Random entre dimensiones
+            //Se inicializa 1 y se reinicia cuando uno se consume
+            //No puede aparecer el los SavedPositions[] (opcional)
+
+            Random rnd = new Random();
+            int energyPositionX = rnd.Next(0, dimension.x);
+            int energyPositionY = rnd.Next(0, dimension.y);
+
+            try
+            {
+                arraySnake[energyPositionX, energyPositionY] = 'e';
+                isEnergy = true;
+                return (energyPositionX, energyPositionY);
+            }
+            catch (Exception ex)
+            {
+                
+                Console.WriteLine(ex.ToString());
+                return (0,0);
+                
+
+            }
         }
 
         private static void GameOver()
         {
             Console.WriteLine("GAME OVER");
-            isOver = true;
+            isGameOver = true;
         }
 
         private static void ImprimirTablero()
