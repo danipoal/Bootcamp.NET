ac5d5cf2c473e8063fe341a573775be8c256be66
danipoal <danipoal.0@gmail.com>
2024-11-29 20:25:09 +0100

    SetUp PAC Desarrollo entorno cliente

---
+++ Front/Pac_Cliente/PAC/piedraPapelTijera.js
@@ -0,0 +1,14 @@
+// Este array no se puede modificar,
+var posibilidades = ["piedra", "papel", "tijera"];
+//    //
+var imgsJugardor = ["piedraJugador.png", "papelJugador.png", "tijeraJugador.png"];
+var imgsOrdenador = ["piedraOrdenador.png", "papelOrdenador.png", "tijeraOrdenador.png"];
+//TODO Sistema de numero partidas y nombre de usuario
+
+
+//TODO Logica de juego, eleccion de la posibilidad vs random machine
+
+
+
+
+//TODO Guardar las partidas que se van jugando hasta que se pulse reset
